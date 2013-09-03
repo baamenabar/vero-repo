1ec79c18c35c692fd76c055b102e7dd3d6e35819
Nikolas <nikoski89@hotmail.com>
2013-09-03 12:24:35 -0400

    Fix nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -750,6 +750,19 @@ var responsiveNav = (function (window, document) {
   return rn;
 })(window, document);
 
+// Responsive Nav
+var navigation = responsiveNav("#site-nav", { // Selector: The ID of the wrapper
+  animate: true, // Boolean: Use CSS3 transitions, true or false
+  transition: 400, // Integer: Speed of the transition, in milliseconds
+  label: "<i class='icon-reorder'></i> Menu", // String: Label for the navigation toggle
+  insert: "before", // String: Insert the toggle before or after the navigation
+  customToggle: "", // Selector: Specify the ID of a custom toggle
+  openPos: "relative", // String: Position of the opened nav, relative or static
+  jsClass: "js", // String: 'JS enabled' class which is added to <html> el
+  init: function(){}, // Function: Init callback
+  open: function(){}, // Function: Open callback
+  close: function(){} // Function: Close callback
+});
 
 // Magnific Popup v0.9.5 by Dmitry Semenov
 // http://bit.ly/magnific-popup#build=image+gallery+retina+imagezoom+fastclick
