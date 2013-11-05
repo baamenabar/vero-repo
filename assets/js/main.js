e516ccf51aa49fe9d07eaef2440f7798b011cb26
Nikolas <nikoski89@hotmail.com>
2013-11-05 11:15:11 -0300

    Fix #15

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+//SVG Fallback
+$(function(){
+  if (!Modernizr.svg) {
+      $('img[src$=svg]').each(function(index, item) {
+          imagePath = $(item).attr('src');
+          $(item).attr('src',imagePath.slice(0,-3)+'png');
+      });
+  }
+});
+
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 /*! NOTE: If you're already including a window.matchMedia polyfill via Modernizr or otherwise, you don't need this part */
 
@@ -941,5 +951,4 @@ $(function() {
       enabled: true
     }
   });
-
 });
